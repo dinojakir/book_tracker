7a58d7c7994ce6451bacd301e423185432e76730
Dino Jakir <dino.jakir.hr@gmail.com>
2026-06-01 20:02:13 +0200

    Supabase configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,25 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, InjectionToken, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../environments/environment';
 import { routes } from './app.routes';
+import { Database } from './db/types';
+
+export const SUPABASE = new InjectionToken<SupabaseClient>('SUPABASE');
+
+export const supabaseProvider = {
+  provide: SUPABASE,
+  useFactory: () =>
+    createClient<Database>(
+      environment.supabaseUrl,
+      environment.supabaseAnonKey
+    )
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    supabaseProvider
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://zsikkrgnxdsmvzyzbsuw.supabase.co',
+  supabaseAnonKey: 'sb_publishable_mDyMrg8hKZRWIl5q8G3nsg_-b2lErU-'
+};
