dc9ecc5a74da6e7bd3c0749338de677953a483af
Dino Jakir <dino.jakir@eucon.com>
2026-06-03 07:48:30 +0200

    Sidebar basic configuration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, inject, signal } from '@angular/core';
 import { SUPABASE } from './app.config';
+import { Sidebar } from './components/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Sidebar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('book_tracker');
