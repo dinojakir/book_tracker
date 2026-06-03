32ef8d198072ea98cfb197b37feae4762f3c2b9e
Dino Jakir <dino.jakir@eucon.com>
2026-06-03 09:22:26 +0200

    Configuration of add area button

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {}
+export class Sidebar {
+  newArea = '';
+
+  addArea() {
+    if (this.newArea.trim()) {
+      console.log('Add area', this.newArea);
+      this.newArea = '';
+    }
+  }
+}
