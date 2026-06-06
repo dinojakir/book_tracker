5daaad6a104d552634a3b914b660ce5cdd005963
Dino Jakir <dino.jakir@eucon.com>
2026-06-06 11:57:18 +0200

    Configuration for adding new area

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,19 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Area, AreaInsert, AreasService } from '../../db/areas.service';
 
 @Component({
   selector: 'app-sidebar',
   imports: [FormsModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
+  standalone: true,
 })
-export class Sidebar {
-  newArea = '';
+export class Sidebar implements OnInit {
+  private readonly areaService = inject(AreasService);
 
-  addArea() {
-    if (this.newArea.trim()) {
-      console.log('Add area', this.newArea);
-      this.newArea = '';
+  allAreas = signal<Area[]>([]);
+  newArea = signal('');
+
+  ngOnInit(): void {
+    this.getAreas();
+  }
+
+  async addArea() {
+    const name = this.newArea().trim();
+
+    if (!name) return;
+
+    const payload: AreaInsert = {
+      name,
+      parent_id: null,
+    };
+
+    try {
+      await this.areaService.create(payload);
+      this.newArea.set('');
+    } catch (error) {
+      console.error(error);
     }
+  }
+
+  async getAreas() {
+    try {
+      const areas = await this.areaService.getAll();
+      this.allAreas.set(areas);
+
+      console.log(areas);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  updateNewArea(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+
+    this.newArea.set(value);
   }
 }
